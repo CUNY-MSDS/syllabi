22719c6730d4bd042b83ad85bbdd0c6e743fffd0
Jason Bryer <jason@bryer.org>
2021-03-04 09:31:35 -0500

    Added Fall 2021 schedule

--- build.R
+++ build.R
@@ -1,8 +1,13 @@
 source('build_syllabi.R')
 
+core_courses <- c('DATA606', 'DATA607', 'DATA608', 'DATA602', 'DATA605', 'DATA621', 'DATA698')
+
 # Spring 2021
-build_syllabi(c('DATA606', 'DATA607'),
+# 622, 624, 609, 620
+build_syllabi(c(core_courses),
 			  semester = 'Spring', year = 2021)
 
-
+# Fall 2021
+build_syllabi(c(core_courses),
+			  semester = 'Fall', year = 2021)
 
