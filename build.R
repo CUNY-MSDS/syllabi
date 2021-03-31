b68df6ef7cfa3b9f583f3afeb22604aba31f5f3a
Jason Bryer <jason@bryer.org>
2021-03-31 15:32:06 -0400

    Update build.R

--- build.R
+++ build.R
@@ -4,8 +4,12 @@ core_courses <- c('DATA606', 'DATA607', 'DATA608', 'DATA602', 'DATA605', 'DATA62
 
 # Spring 2021
 # 622, 624, 609, 620
-build_syllabi(c(core_courses),
+build_syllabi(c(core_courses, 'DATA622', 'DATA624'),
 			  semester = 'Spring', year = 2021)
+
+# Summer 2021
+build_syllabi(c('DATA624', 'DATA612', 'DATA620', 'DATA604),
+			  semester = 'Summer', year = 2021)
 
 # Fall 2021
 build_syllabi(c(core_courses),
